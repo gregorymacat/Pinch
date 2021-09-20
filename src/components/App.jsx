0a748ce584011cfe2d691b2fc3162ae2c5d6a68e
Gregory Macat <gregorymacat@gmail.com>
2021-09-20 15:34:10 -0400

    Fixing eslinter for app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({ state: ' ' });
+
   return (
     <div>
       <h1>Welcome to our Application!</h1>
     </div>
   );
-}
+};
 
 export default App;
