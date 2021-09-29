b8b5f34c290ab0d7eca32f9bdd65947267f74a4c
Gregory Macat <gregorymacat@gmail.com>
2021-09-29 15:37:30 -0400

    Adding barebone component for example

---
+++ src/Components/01.Homepage/ToolsOffered.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+function ToolsOffered() {
+  return (
+    <>
+      <div className="tools-offered-goals">
+        <span>Description of the goals tracker goes here</span>
+      </div>
+      <div className="tools-offered-sub-tracker">
+        <span>Description of the subscriptions tracker goes here</span>
+      </div>
+      <div className="tools-offered-budget">
+        <span>Description of the budget tracker goes here</span>
+      </div>
+      <div className="tools-offered-credit">
+        <span>Description of the budget tracker goes here</span>
+      </div>
+    </>
+  );
+}
