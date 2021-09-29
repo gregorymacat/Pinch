9028023efcf43492677ad4152691fb9ec6aa2d77
Gregory Macat <gregorymacat@gmail.com>
2021-09-29 17:23:55 -0400

    Added lint script, removed files, changed webpack, setting up my scss

--- src/Components/01.Homepage/ToolsOffered.tsx
+++ src/Components/01.Homepage/ToolsOffered.tsx
@@ -2,19 +2,25 @@ import * as React from 'react';
 
 function ToolsOffered() {
   return (
-    <>
-      <div className="tools-offered-goals">
+    <div className="tools-offered">
+      <div className="goals">
+        <img src="/assets/images/noimageavailable.jpg" alt="Visual of the goals tracker" className="example-image" />
         <span>Description of the goals tracker goes here</span>
       </div>
-      <div className="tools-offered-sub-tracker">
+      <div className="sub-tracker">
         <span>Description of the subscriptions tracker goes here</span>
+        <img src="/assets/images/noimageavailable.jpg" alt="Visual of the subscriptions tracker" className="example-image" />
       </div>
-      <div className="tools-offered-budget">
+      <div className="budget">
+        <img src="/assets/images/noimageavailable.jpg" alt="Visual of the budget tracker" className="example-image" />
         <span>Description of the budget tracker goes here</span>
       </div>
-      <div className="tools-offered-credit">
-        <span>Description of the budget tracker goes here</span>
+      <div className="credit">
+        <span>Description of the credit payment goes here</span>
+        <img src="/assets/images/noimageavailable.jpg" alt="Visual of the credit payment" className="example-image" />
       </div>
-    </>
+    </div>
   );
 }
+
+export default ToolsOffered;
